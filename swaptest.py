f9f8bfbc7881835f8dbf63f0d6c089d7988c0358
xalien10 <ikhtiarcse10ruet@gmail.com>
2017-02-08 22:41:49 +0600

    Create swaptest.py

---
+++ swaptest.py
@@ -0,0 +1,11 @@
+'''
+This small program clarifies the swap technique of the python. 
+'''
+
+a = 25
+b = 35
+
+print("Before swap: a="+str(a)+" & b="+str(b))
+
+a,b=b,a
+print("After swap: a="+str(a)+" & b="+str(b))
